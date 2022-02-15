a2eea9df8e073ec110e5a6b7e7d084feb17bc445
BreynerM <bmoncadapr@uninpahu.edu.co>
2022-02-15 10:34:59 -0500

    first commit

---
+++ aritmetica.py
@@ -0,0 +1,5 @@
+def sumDosNum(n1,n2):
+    return n1+n2
+
+def restDosNum(n1,n2):
+    return n1-n2
---
+++ index.py
@@ -0,0 +1,7 @@
+
+import aritmetica as a
+import text as t
+
+print(a.sumDosNum(3,4))
+print(a.restDosNum(8,3))
+print(t.concat("hola","mundo"))
