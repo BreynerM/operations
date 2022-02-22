d875dcd86cb1cbba49e234b3f92dbecf2248a8b4
j12jonathan <jguzmansa@uninpahu.edu.co>
2022-02-22 10:45:21 -0500

    clase 22

--- aritmetica.py
+++ aritmetica.py
@@ -1,5 +1,36 @@
 def sumDosNum(n1,n2):
     return n1+n2
 
+
 def restDosNum(n1,n2):
     return n1-n2
+
+
+def MultiplicacionNum(n1,n2):
+    return n1*n2
+
+
+def DividirNum(n1,n2):
+    return n1/n2
+
+
+def MediaCautroNum(n1,n2,n3,n4):
+    resultado = n1 + n2 + n3 + n4 / 4
+    return resultado
+
+
+def AreaTriangulo(b,h):
+    area = b * h / 2
+    return area
+    
+
+def AreaCirculo(r):
+    area = PI * r**2
+    return area
+
+
+PI = 3.1416
+
+
+
+    
